1bbd8e44f8aa5c44a2c0eba1610c73a115298fd9
Dean Panayotov <dpanayotov@melontech.com>
2014-11-03 12:19:52 +0200

    Fixed color hue values. Now using the color objects in the fillColors function.

--- landing-page.js
+++ landing-page.js
@@ -1,12 +1,20 @@
-var BASE_COLORS = 
-	new Color(207, 41, 12, 3, 3, 3),
-	new Color(216, 92, 16, 3, 3, 3),
-	new Color(224, 94, 24, 3, 3, 3),
-	new Color(234, 64, 44, 3, 3, 3),
-	new Color(6  , 64, 58, 3, 3, 3),
-	new Color(12 , 93, 60, 3, 3, 3),
-	new Color(16 , 64, 84, 3, 3, 3),
-	new Color(15 , 64, 88, 3, 3, 3),
+var BASE_COLORS = [
+	// new Color(207, 41, 12, 3, 3, 3),
+	// new Color(216, 92, 16, 3, 3, 3),
+	// new Color(224, 94, 24, 3, 3, 3),
+	// new Color(234, 64, 44, 3, 3, 3),
+	// new Color(6  , 64, 58, 3, 3, 3),
+	// new Color(12 , 93, 60, 3, 3, 3),
+	// new Color(16 , 64, 84, 3, 3, 3),
+	// new Color(15 , 64, 88, 3, 3, 3),
+	new Color(292, 41, 12, 3, 3, 3),
+	new Color(305, 92, 16, 3, 3, 3),
+	new Color(316, 94, 24, 3, 3, 3),
+	new Color(330, 64, 44, 3, 3, 3),
+	new Color(8  , 64, 58, 3, 3, 3),
+	new Color(17 , 93, 60, 3, 3, 3),
+	new Color(23 , 64, 84, 3, 3, 3),
+	new Color(21 , 64, 88, 3, 3, 3),
 ];
 
 function Color(h, s, l, hvar, svar, lvar){
@@ -19,11 +27,11 @@ function Color(h, s, l, hvar, svar, lvar){
 
 	this.getColor = function(){
 		return "hsl("+
-			(h+random(hvar*-1,hvar))+
+			(h+random2(hvar*-1,hvar))+
 			","+ 
-			(s+random(svar*-1,svar))+
+			(s+random2(svar*-1,svar))+
 			"%,"+
-			(l+random(lvar*-1,lvar))+
+			(l+random2(lvar*-1,lvar))+
 			"%)";
 	}
 };
@@ -44,7 +52,7 @@ function random(range){
 	return Math.floor(Math.random()*(range - 1));
 }
 
-function random(min, max){
+function random2(min, max){
 	if(min > max){
 		var t = max;
 		max = min;
@@ -70,7 +78,8 @@ function insertCells(){
 function fillColors(){
 	for(i = 0; i < CANVAS_SIZE_IN_PIXELS; i++){
 		for(j = 0; j < CANVAS_SIZE_IN_PIXELS; j++){
-			cells[i][j].setBackgroundColor("rgba("+random(255)+","+random(255)+","+random(255)+",1)");
+			//cells[i][j].setBackgroundColor("rgba("+random(255)+","+random(255)+","+random(255)+",1)");
+			cells[i][j].setBackgroundColor(BASE_COLORS[random(BASE_COLORS.length)].getColor());
 		}
 	}
 }
