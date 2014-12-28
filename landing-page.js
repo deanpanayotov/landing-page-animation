ca769827523f3eb2580e05c8225b2bb73ec8512e
Dean Panayotov <deanpanayotov@gmail.com>
2014-12-28 23:52:52 +0200

    Minimal syntax changes, pointed out by WebStorm.

--- landing-page.js
+++ landing-page.js
@@ -14,16 +14,10 @@ var BASE_COLORS = [
 	new Color(8  , 64, 58, 3, 3, 3),
 	new Color(17 , 93, 60, 3, 3, 3),
 	new Color(23 , 64, 84, 3, 3, 3),
-	new Color(21 , 64, 88, 3, 3, 3),
+	new Color(21 , 64, 88, 3, 3, 3)
 ];
 
 function Color(h, s, l, hvar, svar, lvar){
-	this.h = h;
-	this.s = s;
-	this.l = l;
-	this.hvar = hvar;
-	this.svar = svar;
-	this.lvar = lvar;
 
 	this.getColor = function(){
 		return "hsl("+
@@ -34,7 +28,7 @@ function Color(h, s, l, hvar, svar, lvar){
 			(l+random2(lvar*-1,lvar))+
 			"%)";
 	}
-};
+}
 
 
 var CANVAS_SIZE_IN_PIXELS = 20;
@@ -61,31 +55,29 @@ function random2(min, max){
 	return Math.floor(Math.random() * (max - min) + min);
 }
 
-var table = document.getElementById('table');
-var rows = new Array(CANVAS_SIZE_IN_PIXELS);
 var cells = new Array(CANVAS_SIZE_IN_PIXELS);
 
 function insertCells(){
-	for(i = 0; i < CANVAS_SIZE_IN_PIXELS; i++){
+    var table = document.getElementById('table');
+    var rows = new Array(CANVAS_SIZE_IN_PIXELS);
+    for(var i = 0; i < CANVAS_SIZE_IN_PIXELS; i++){
 		rows[i]	= table.insertRow(table.rows.length);
 		cells[i] = new Array(CANVAS_SIZE_IN_PIXELS);
-		for(j = 0; j < CANVAS_SIZE_IN_PIXELS; j++){
+		for(var j = 0; j < CANVAS_SIZE_IN_PIXELS; j++){
 			cells[i][j] = new Cell(rows[i].insertCell(j));
 		}
 	}
 }
 
 function fillColors(){
-	for(i = 0; i < CANVAS_SIZE_IN_PIXELS; i++){
-		for(j = 0; j < CANVAS_SIZE_IN_PIXELS; j++){
-			//cells[i][j].setBackgroundColor("rgba("+random(255)+","+random(255)+","+random(255)+",1)");
+	for(var i = 0; i < CANVAS_SIZE_IN_PIXELS; i++){
+		for(var j = 0; j < CANVAS_SIZE_IN_PIXELS; j++){
 			cells[i][j].setBackgroundColor(BASE_COLORS[random(BASE_COLORS.length)].getColor());
 		}
 	}
 }
 
 function Cell(cell){
-	this.cell = cell;
 	this.active = false;
 	this.backgroundColor = "#FCC347";
 	cell.style.backgroundColor = this.backgroundColor;
